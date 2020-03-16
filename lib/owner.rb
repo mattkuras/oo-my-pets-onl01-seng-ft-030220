5c80b518302e0553dba8789b63ce185badf7a11c
Matt Kuras <mattkuras@gmail.com>
2020-03-16 21:14:08 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,7 +3,7 @@ class Owner
   
   attr_reader :name, :species 
   @@all = []
-  @pets = 
+  @pets = [self.cats, self.dogs].flatten 
   def initialize(name, species = "human")
     @name = name  
     @species = species 
