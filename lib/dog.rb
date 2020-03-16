b716e8e4db04528a8981e57aed22ea9d8a18ca42
Matt Kuras <mattkuras@gmail.com>
2020-03-16 19:21:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
   
+  attr_accessor :owner, :mood
+  attr_reader :name 
+  
+  @@all_dogs = []
+  
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @owner = owner 
+    @mood = mood 
+    @@all_dogs << self 
+  end
+  
 end
