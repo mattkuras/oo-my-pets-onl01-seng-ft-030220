a1386256e2593997998ed79713c3e4b0bd8099d2
Matt Kuras <mattkuras@gmail.com>
2020-03-16 19:01:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,13 @@
 class Owner
   
-  attr_reader :name  
-  attr_reader :species 
+  attr_reader :name, :species 
   
   def initialize(name, species = "human")
     @name = name  
     @species = species 
   end
   
+  def say_species 
+    puts "I am a #{self.species}"
+  end
 end
